ce78105a3b1688749823403b20b547e48b9a02eb
rapunzel1far <honeyfar@yandex.ru>
2023-01-14 23:00:34 +0300

    14/01

--- zastavka.py
+++ zastavka.py
@@ -9,14 +9,30 @@ pygame.init()
 fontUI = pygame.font.Font(None, 30)
 
 
-def load_image(name, colorkey='black'):
-    fullname = os.path.join('data', name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    return image
+# def load_image(name, colorkey='black'):
+    # fullname = os.path.join('data', name)
+    # if not os.path.isfile(fullname):
+    #     print(f"Файл с изображением '{fullname}' не найден")
+    #     sys.exit()
+    # image = pygame.image.load(fullname)
+    # return image
 
+    def load_image(name, colorkey=None):
+        fullname = os.path.join('data', name)
+        # если файл не существует, то выходим
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        # прозрачный цвет
+        if colorkey is not None:
+            image = image.convert()
+            if colorkey == -1:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey)
+        else:
+            image = image.convert_alpha()
+        return image
 
 def terminate():
     pygame.quit()
